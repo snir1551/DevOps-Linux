1095da4013185aded17c92ea861a655bed444dc5
snir1551 <58264273+snir1551@users.noreply.github.com>
2025-06-17 15:24:24 +0300

    Delete week7_practice directory

--- week7_practice/backend/User.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const userSchema = new mongoose.Schema({
-  name: String,
-  email: String,
-  createdAt: { type: Date, default: Date.now }
-});
-
-export default mongoose.model('User', userSchema);
