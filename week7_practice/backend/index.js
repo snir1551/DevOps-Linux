38c45d3fcdb2f1e0dd2593cac29bdb2937da4af6
snir1551 <58264273+snir1551@users.noreply.github.com>
2025-06-17 21:34:33 +0300

    Update index.js

--- week7_practice/backend/index.js
+++ week7_practice/backend/index.js
@@ -46,6 +46,6 @@ app.get('/', (req, res) => {
 });
 
 
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running at http://0.0.0.0:${PORT}`);
 });
